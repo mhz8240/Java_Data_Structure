42b31f8a0d0813e0fc53cd64a946bfccd97f18c4
mhz8240 <michaelzheng119@gmail.com>
2021-07-08 21:34:36 -0700

    added remove and resize

--- HashMap/src/com/michael/dynamicarray/Main.java
+++ HashMap/src/com/michael/dynamicarray/Main.java
@@ -8,6 +8,8 @@ public class Main {
         myHashTable.put("John", "382-0879");
         myHashTable.put("Jane", "356-6215");
         myHashTable.put("Joe", "796-5423");
+        myHashTable.put("Jay", "706-5223");
+        myHashTable.put("Michael", "746-5498");
         System.out.println(myHashTable.toString());
         System.out.println(myHashTable.get("John"));
         System.out.println(myHashTable.get("john"));
--- HashMap/src/com/michael/dynamicarray/MyHashTable.java
+++ HashMap/src/com/michael/dynamicarray/MyHashTable.java
@@ -4,20 +4,23 @@ import java.util.Arrays;
 
 public class MyHashTable {
     private Data[] data;
-    private int INITIAL_VALUE = 16;
+    private int bucketSize = 4;
+    private int size = 0;
 
     public MyHashTable() {
-        this.data = new Data[INITIAL_VALUE];
+        this.data = new Data[bucketSize];
     }
 
     private class Data {
         String key;
         String value;
+        final int hashCode;
         Data next;
-        public Data(String key, String value) {
+        public Data(String key, String value, int hashCode) {
             this.key = key;
             this.value = value;
             this.next = null;
+            this.hashCode = hashCode;
         }
 
         @Override
@@ -25,32 +28,61 @@ public class MyHashTable {
             return "Data{" +
                     "key='" + key + '\'' +
                     ", value='" + value + '\'' +
+                    ", hashCode=" + hashCode +
+                    ", next=" + next +
                     '}';
         }
     }
+
+
     public void put(String key, String value) {
         int index = getIndex(key);
+        int hashCode = getHashCode(key);
         if (data[index] == null) {
-            data[index] = new Data(key, value);
+            data[index] = new Data(key, value, hashCode);
+            size++;
+            if (getLoadFactor() >= 0.7) {
+                resize();
+            }
             return;
         }
         Data currentData = data[index];
         while (currentData.next != null) {
+            if (key.equals(data[index].key) && hashCode == data[index].hashCode) {
+                currentData.value = value;
+                return;
+            }
             currentData = currentData.next;
         }
-        currentData.next = new Data(key, value);
-    }
-    private int getIndex(String key) {
-        return (key.hashCode() & 0x7fffffff) % INITIAL_VALUE;
+        currentData.next = new Data(key, value, hashCode);
+        size++;
+        if (getLoadFactor() >= 0.7) {
+            resize();
+        }
     }
 
-    @Override
-    public String toString() {
-        return "MyHashTable{" +
-                "data=" + Arrays.toString(data) +
-                ", INITIAL_VALUE=" + INITIAL_VALUE +
-                '}';
+    private void resize() {
+        bucketSize *= 2;
+        Data[] temp = data;
+        data = new Data[bucketSize];
+        size = 0;
+        for (int i = 0; i < bucketSize/2; i++) {
+            Data obj = temp[i];
+            while(obj != null) {
+                put(obj.key, obj.value);
+                obj = obj.next;
+            }
+        }
     }
+    private int getHashCode(String key) {
+        return Math.abs(key.hashCode());
+    }
+
+    private int getIndex(String key) {
+        return getHashCode(key) % bucketSize;
+    }
+
+
 
     public String get(String key) {
         int index = getIndex(key);
@@ -67,5 +99,49 @@ public class MyHashTable {
         }
         System.out.println("Can't find the given key");
         return null;
+    }
+
+    public void remove(String key) {
+        int index = getIndex(key);
+        if (data[index] == null) {
+            System.out.println("Can't find the given key");
+            return;
+        }
+        if (data[index].key.equals(key)) {
+            data[index] = data[index].next;
+            size--;
+            return;
+        }
+        Data currentData = data[index];
+        while (currentData.next != null) {
+            if (currentData.next.key.equals(key)) {
+                currentData.next = currentData.next.next;
+                size--;
+                return;
+            }
+        }
+        System.out.println("Can't find the given key");
+        return;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    private double getLoadFactor() {
+        return (double) getSize() / bucketSize;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashTable{" +
+                "data=" + Arrays.toString(data) +
+                ", bucketSize=" + bucketSize +
+                ", size=" + size +
+                '}';
     }
 }
